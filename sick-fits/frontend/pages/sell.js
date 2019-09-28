89791807e7e777b301ce6dc00fae0356f3eafc56
Miguel Machado <miiguellmp@gmail.com>
2019-09-27 23:53:08 -0300

    custom _app.js layout

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,11 +1,6 @@
-import Link from "next/link"; // using link from next to server-side
-
 const Sell = props => (
   <div>
     <p>Hey!</p>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
   </div>
 );
 
